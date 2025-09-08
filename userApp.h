5eee5fde2bdf28e451105617ea5a95a7330508e8
oskark22 <114870670+oskark22@users.noreply.github.com>
2025-09-08 15:50:02 +0300

    Add files via upload

---
+++ userApp.h
@@ -0,0 +1,14 @@
+/*
+ * userApp.h
+ *
+ *  Created on: Sep 18, 2024
+ *      Author: G00397054@atu.ie
+ */
+
+#ifndef INC_USERAPP_H_
+#define INC_USERAPP_H_
+
+#include "main.h"
+
+void userApp();
+#endif /* INC_USERAPP_H_ */
